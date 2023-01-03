6a30c9a4d64d52d22864aa808ea94b0a916c843a
Hannah Beattie <hannahdbeattie@gmail.com>
2023-01-03 20:27:12 +1300

    add circles!! fix

--- pages/draw.js
+++ pages/draw.js
@@ -13,7 +13,7 @@ const ReactP5Wrapper = dynamic(() => import('react-p5-wrapper').then((mod) => mo
 class Tree {
 	constructor({ p5 }) {
 		this.p5 = p5
-		const edges = 0
+		const edges = 100
 		this.center = {
 			x: p5.random(edges, p5.width - edges),
 			y: p5.random(edges, p5.height - edges),
@@ -21,36 +21,36 @@ class Tree {
 	}
 	draw = () => {
 		const p5 = this.p5
-		let scale = p5.random(3, 30)
-		let resolution = p5.random(0.001, 0.008)
-		let numPoints = p5.random(100, 400)
-		let radius = p5.random(20, 100)
-		let numRings = p5.random(10, 100)
+		let scale = p5.random(8, 10)
+		let resolution = p5.random(0.01, 0.08)
+		let numPoints = p5.random(20, 100)
+		let radius = p5.random(40, 90)
+		// let numRings = p5.random(10, 100)
+		let numRings = p5.random(5, 20)
 		let r
 		let n
 		let a
 		let y
 		let x
 
-		p5.stroke(10, 40)
 		p5.push()
 		p5.translate(this.center.x, this.center.y)
 
 		for (r = 0; r < radius; r += radius / numRings) {
 			p5.beginShape()
 			for (
-				a = -p5.TAU / numPoints;
-				a < p5.TAU + p5.TAU / numPoints;
+				a = -p5.TAU - p5.TAU / numPoints;
+				a <= p5.TAU + p5.TAU / numPoints;
 				a += p5.TAU / numPoints
 			) {
 				x = r * p5.cos(a)
 				y = r * p5.sin(a)
 
-				n = p5.map(p5.noise(x * resolution, y * resolution), 0, 2, -scale, scale)
+				n = p5.map(p5.noise(x * resolution, y * resolution), 0, 3, -scale, scale)
 
 				p5.curveVertex(x + n, y + n)
 
-				if (p5.random() > 0.75 - 0.25 * p5.sin(r)) {
+				if (p5.random() > 0.75 - 0.01 * p5.sin(r)) {
 					p5.endShape()
 					p5.beginShape()
 				}
@@ -62,31 +62,37 @@ class Tree {
 }
 
 function sketch(p5) {
+	let trees = []
+
 	function reset() {
 		p5.clear()
 		p5.background(240, 240, 240, 230)
 		let ranStoke = p5.random(0.1, 1)
 		p5.strokeWeight(ranStoke)
 		p5.noFill()
+
+		const numTree = p5.random(4, 15)
+		// const numTree = 5
+		trees = []
+		for (let ii = 0; ii < numTree; ii++) {
+			trees.push(new Tree({ p5 }))
+		}
+
 		for (const tree of trees) {
 			tree.draw()
 		}
 	}
-	let trees = []
+
 	p5.setup = () => {
 		p5.createCanvas(800, 500)
-
-		const numTree = p5.random(1, 5)
-		for (let ii = 0; ii < numTree; ii++) {
-			trees.push(new Tree({ p5 }))
-		}
-		p5.noLoop()
+		// p5.noLoop()
 		p5.mousePressed = () => {
 			reset()
 		}
 	}
 
 	p5.draw = () => {
+		p5.frameRate(0.5)
 		reset()
 	}
 }
