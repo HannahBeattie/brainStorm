4fc1b1e33291b7796a1c3e6e3458f0acbbbf7c30
Hannah Beattie <hannahdbeattie@gmail.com>
2023-01-03 16:02:57 +1300

    four to 40

--- pages/draw.js
+++ pages/draw.js
@@ -13,7 +13,7 @@ const ReactP5Wrapper = dynamic(() => import('react-p5-wrapper').then((mod) => mo
 class Tree {
 	constructor({ p5 }) {
 		this.p5 = p5
-		const edges = 4
+		const edges = 40
 		this.center = {
 			x: p5.random(edges, p5.width - edges),
 			y: p5.random(edges, p5.height - edges),
@@ -21,16 +21,18 @@ class Tree {
 	}
 	draw = () => {
 		const p5 = this.p5
-		let scale = p5.random(3, 30)
+		let scale = p5.random(9, 90)
 		let resolution = p5.random(0.001, 0.008)
-		let numPoints = p5.random(100, 400)
-		let radius = p5.random(20, 100)
-		let numRings = p5.random(10, 100)
+		let numPoints = p5.random(9, 900)
+		let radius = p5.random(9, 90)
+		let numRings = p5.random(9, 90)
 		let r
 		let n
 		let a
 		let y
 		let x
+
+		p5.stroke(10, 40)
 		p5.push()
 		p5.translate(this.center.x, this.center.y)
 
@@ -41,10 +43,10 @@ class Tree {
 				a < p5.TAU + p5.TAU / numPoints;
 				a += p5.TAU / numPoints
 			) {
-				x = 200 + r * p5.cos(a)
-				y = 200 + r * p5.sin(a)
+				x = r * p5.cos(a)
+				y = r * p5.sin(a)
 
-				n = p5.map(p5.noise(x * resolution, y * resolution), 0, 1, -scale, scale)
+				n = p5.map(p5.noise(x * resolution, y * resolution), 0, 2, -scale, scale)
 
 				p5.curveVertex(x + n, y + n)
 
@@ -62,7 +64,10 @@ class Tree {
 function sketch(p5) {
 	function reset() {
 		p5.clear()
-		p5.background(220)
+		p5.background(240, 240, 240, 230)
+		let ranStoke = p5.random(0.1, 1)
+		p5.strokeWeight(ranStoke)
+		p5.noFill()
 		for (const tree of trees) {
 			tree.draw()
 		}
@@ -71,14 +76,10 @@ function sketch(p5) {
 	p5.setup = () => {
 		p5.createCanvas(800, 500)
 
-		p5.stroke(0)
-		p5.strokeWeight(1)
-		p5.noFill()
-		const numTree = 20
+		const numTree = p5.random(4, 30)
 		for (let ii = 0; ii < numTree; ii++) {
 			trees.push(new Tree({ p5 }))
 		}
-
 		p5.noLoop()
 		p5.mousePressed = () => {
 			reset()
